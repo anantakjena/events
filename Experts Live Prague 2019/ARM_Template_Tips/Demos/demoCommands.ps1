e9646cdb08806931ee62af330e8a9192d3410878
Sam Cogan <sam.cogan@willistowerswatson.com>
2019-11-19 13:56:10 +0000

    add whatiff

--- Experts Live Prague 2019/ARM_Template_Tips/Demos/demoCommands.ps1
+++ Experts Live Prague 2019/ARM_Template_Tips/Demos/demoCommands.ps1
@@ -2,10 +2,10 @@
 
 
 ### 3_Outputs
-New-AzResourceGroupDeployment -Name "3_outputs_1" -ResourceGroupName "igniteARMDemos" -TemplateFile .\3_Outputs\simpleOutput.json
-$x=New-AzResourceGroupDeployment -Name "3_outputs_2" -ResourceGroupName "igniteARMDemos" -TemplateFile .\3_Outputs\objectOutput.json
+New-AzResourceGroupDeployment -Name "3_outputs_1" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\simpleOutput.json
+$x=New-AzResourceGroupDeployment -Name "3_outputs_2" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\objectOutput.json
 $y= [Newtonsoft.Json.JsonConvert]::SerializeObject($x.Outputs.publicIP.value, [Newtonsoft.Json.Formatting]::Indented) | ConvertFrom-Json
-New-AzResourceGroupDeployment -Name "3_outputs_3" -ResourceGroupName "igniteARMDemos" -TemplateFile .\3_Outputs\Variables.json
+New-AzResourceGroupDeployment -Name "3_outputs_3" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\Variables.json
 
 ### 4_NestedTemplates
 
@@ -13,8 +13,13 @@ $x=New-AzResourceGroupDeployment -Name "4_nested_1" -ResourceGroupName "crossSub
 
 ### 6_conditions
 
-New-AzResourceGroupDeployment -Name "6_conditions_1" -ResourceGroupName "igniteARMDemos" -TemplateFile .\6_Conditions\if.json -TemplateParameterFile .\6_Conditions\if.params.json
+New-AzResourceGroupDeployment -Name "6_conditions_1" -ResourceGroupName "ARMDemos" -TemplateFile .\6_Conditions\if.json -TemplateParameterFile .\6_Conditions\if.params.json
 
 ### 8_sublevel
 
-New-AzureRmDeployment -Name "8_sublevel_01" -Location "West Europe" -TemplateFile .\8_SubLevelDeployments\template.json -TemplateParameterFile .\8_SubLevelDeployments\template.parameters.json
+New-AzDeployment -Name "8_sublevel_01" -Location "West Europe" -TemplateFile .\8_SubLevelDeployments\template.json -TemplateParameterFile .\8_SubLevelDeployments\template.parameters.json
+
+
+### 9 Testing 
+
+.\9_Testing\azuredeploy.tests.ps1
--- Experts Live Prague 2019/Kubernetes_Azure/Demos/5_Keda/GenerateMessages.ps1
+++ Experts Live Prague 2019/Kubernetes_Azure/Demos/5_Keda/GenerateMessages.ps1
@@ -1,3 +1,4 @@
+#! /usr/bin/pwsh
 param (
     [int]$count= 9 
 
