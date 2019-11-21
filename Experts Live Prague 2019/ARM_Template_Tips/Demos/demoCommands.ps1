aa88debd243935263c6dfd5e6d3bf16a0bceaaa2
Sam Cogan <sam.cogan@willistowerswatson.com>
2019-11-21 10:21:25 +0100

    update demos

--- Experts Live Prague 2019/ARM_Template_Tips/Demos/demoCommands.ps1
+++ Experts Live Prague 2019/ARM_Template_Tips/Demos/demoCommands.ps1
@@ -2,10 +2,10 @@
 
 
 ### 3_Outputs
-New-AzResourceGroupDeployment -Name "3_outputs_1" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\simpleOutput.json
-$x=New-AzResourceGroupDeployment -Name "3_outputs_2" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\objectOutput.json
+New-AzResourceGroupDeployment -Name "3_outputs_1" -ResourceGroupName "ARMDemosOutput" -TemplateFile .\3_Outputs\simpleOutput.json
+$x=New-AzResourceGroupDeployment -Name "3_outputs_2" -ResourceGroupName "ARMDemosARMDemosOutput" -TemplateFile .\3_Outputs\objectOutput.json
 $y= [Newtonsoft.Json.JsonConvert]::SerializeObject($x.Outputs.publicIP.value, [Newtonsoft.Json.Formatting]::Indented) | ConvertFrom-Json
-New-AzResourceGroupDeployment -Name "3_outputs_3" -ResourceGroupName "ARMDemos" -TemplateFile .\3_Outputs\Variables.json
+New-AzResourceGroupDeployment -Name "3_outputs_3" -ResourceGroupName "ARMDemosARMDemosOutput" -TemplateFile .\3_Outputs\Variables.json
 
 ### 4_NestedTemplates
 
@@ -13,7 +13,7 @@ $x=New-AzResourceGroupDeployment -Name "4_nested_1" -ResourceGroupName "crossSub
 
 ### 6_conditions
 
-New-AzResourceGroupDeployment -Name "6_conditions_1" -ResourceGroupName "ARMDemos" -TemplateFile .\6_Conditions\if.json -TemplateParameterFile .\6_Conditions\if.params.json
+New-AzResourceGroupDeployment -Name "6_conditions_1" -ResourceGroupName "ARMDemosCondition" -TemplateFile .\6_Conditions\if.json -TemplateParameterFile .\6_Conditions\if.params.json
 
 ### 8_sublevel
 
@@ -23,3 +23,9 @@ New-AzDeployment -Name "8_sublevel_01" -Location "West Europe" -TemplateFile .\8
 ### 9 Testing 
 
 .\9_Testing\azuredeploy.tests.ps1
+
+###11  What IF
+
+New-AzDeploymentWhatIf -Name "testing" -ResourceGroupName "ARMDemosCondition" -TemplateFile .\9_Testing\AzureDeploy.json -TemplateParameterFile .\9_Testing\AzureDeploy.parameters.json -ScopeType ResourceGroup
+
+New-AzDeploymentWhatIf -Name "6_conditions_1" -ResourceGroupName "ARMDemosCondition" -TemplateFile .\6_Conditions\if.json -TemplateParameterFile .\6_Conditions\if.params.json -ScopeType ResourceGroup -Mode Complete
